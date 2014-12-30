eee8809db8cabaeab8acdb24ea6489d5e39d2f7f
John K.E. Wilson <wilsonfiifi@gmail.com>
2014-12-30 19:15:40 +0000

    fixed Redis timeout bug

--- statestore/redis/redis.go
+++ statestore/redis/redis.go
@@ -23,9 +23,15 @@ const (
 
 type StateStore struct {
 	client *redis.Client
+	config *Config
 }
 
 func (s *StateStore) TokenSet(token, user string, timeout int64) error {
+	// check connection
+	if err := s.checkConnection(); err != nil {
+		return err
+	}
+
 	key := fmt.Sprintf("%s:%s", TokenPrefix, token)
 	r := s.client.Cmd("SETEX", key, timeout, user)
 	if r.Err != nil {
@@ -35,6 +41,11 @@ func (s *StateStore) TokenSet(token, user string, timeout int64) error {
 }
 
 func (s *StateStore) TokenGet(token string) (string, error) {
+	// check connection
+	if err := s.checkConnection(); err != nil {
+		return "", err
+	}
+
 	key := fmt.Sprintf("%s:%s", TokenPrefix, token)
 	r := s.client.Cmd("GET", key)
 	if r.Err != nil {
@@ -44,6 +55,11 @@ func (s *StateStore) TokenGet(token string) (string, error) {
 }
 
 func (s *StateStore) CacheSet(id, value string, timeout int64) error {
+	// check connection
+	if err := s.checkConnection(); err != nil {
+		return err
+	}
+
 	key := fmt.Sprintf("%s:%s", CachePrefix, id)
 	r := s.client.Cmd("SETEX", key, timeout, value)
 	if r.Err != nil {
@@ -53,6 +69,11 @@ func (s *StateStore) CacheSet(id, value string, timeout int64) error {
 }
 
 func (s *StateStore) CacheGet(id string) (string, error) {
+	// check connection
+	if err := s.checkConnection(); err != nil {
+		return "", err
+	}
+
 	key := fmt.Sprintf("%s:%s", CachePrefix, id)
 	r := s.client.Cmd("GET", key)
 	if r.Err != nil {
@@ -62,8 +83,55 @@ func (s *StateStore) CacheGet(id string) (string, error) {
 }
 
 func (s *StateStore) ClearAll() error {
+	// check connection
+	if err := s.checkConnection(); err != nil {
+		return err
+	}
+
 	r := s.client.Cmd("FLUSHDB")
 	if r.Err != nil {
+		return r.Err
+	}
+	return nil
+}
+
+func (s *StateStore) connect() error {
+	client, err := redis.DialTimeout(
+		"tcp",
+		s.config.Address,
+		s.config.Timeout*time.Second,
+	)
+	if err != nil {
+		return err
+	}
+
+	// check if passowrd given and authenticate accordingly
+	if len(s.config.Password) > 0 {
+		r := client.Cmd("AUTH", s.config.Password)
+		if r.Err != nil {
+			return r.Err
+		}
+	}
+
+	// select the database
+	r := client.Cmd("SELECT", s.config.Database)
+	if r.Err != nil {
+		return r.Err
+	}
+
+	// set client
+	s.client = client
+	return nil
+}
+
+func (s *StateStore) checkConnection() error {
+	// ping server
+	r := s.client.Cmd("PING")
+	if r.Err != nil {
+		// check if closed connection error
+		if r.Err.Error() == "use of closed network connection" {
+			return s.connect()
+		}
 		return r.Err
 	}
 	return nil
@@ -75,28 +143,10 @@ func (s *StateStore) Start(config string) error {
 	if err != nil {
 		return err
 	}
+	s.config = &c
 
-	client, err := redis.DialTimeout("tcp", c.Address, c.Timeout*time.Second)
-	if err != nil {
-		return err
-	}
-
-	// check if passowrd given and authenticate accordingly
-	if len(c.Password) > 0 {
-		r := client.Cmd("AUTH", c.Password)
-		if r.Err != nil {
-			return r.Err
-		}
-	}
-
-	// select the database
-	r := client.Cmd("SELECT", c.Database)
-	if r.Err != nil {
-		return r.Err
-	}
-
-	s.client = client
-	return nil
+	// connect
+	return s.connect()
 }
 
 func NewStateStore() *StateStore {
